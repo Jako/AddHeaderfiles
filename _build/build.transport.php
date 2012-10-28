93776660ef9c9dab5808354d8a01479ff20874d1
Thomas Jakobi <thomas.jakobi@partout.info>
2012-10-28 10:57:28 +0100

    Small Typo

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * AddHeaderfiles
- *
+ * 
  * Copyright 2008-2012 by Thomas Jakobi <thomas.jakobi@partout.info>
- *
+ * 
  * AddHeaderfiles is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
  * Software Foundation; either version 2 of the License, or (at your option) any 
  * later version.
  *
- * AddHeaderfiles is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * AddHeaderfiles is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * Rowboat; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * AddHeaderfiles; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * @package addheaderfiles
  * @subpackage build
@@ -107,7 +107,7 @@ $vehicle = $builder->createVehicle($category, $attr);
 $modx->log(modX::LOG_LEVEL_INFO, 'Adding file resolvers to plugin...');
 $vehicle->resolve('file', array(
 	'source' => $sources['source_core'],
-	'target' => "return MODX_CORE_PATH . 'components/';",
+	'target' => "return MODX_CORE_PATH . 'components/';"
 ));
 $builder->putVehicle($vehicle);
 
